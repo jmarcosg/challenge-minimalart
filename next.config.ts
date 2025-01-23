095e1c34c2687c4f159ccc4cc83c420c55285c66
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2025-01-23 13:40:59 -0300

    🔧 update deprecated code

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ["raw.githubusercontent.com"],
-  }
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
