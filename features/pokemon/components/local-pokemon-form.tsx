d4eb5893455e2062961ceb81ef2e909439ed8a4b
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2025-01-24 11:19:12 -0300

    💄 adjust layout of search input and local pokemon form

--- features/pokemon/components/local-pokemon-form.tsx
+++ features/pokemon/components/local-pokemon-form.tsx
@@ -77,7 +77,7 @@ export function LocalPokemonForm() {
   return (
     <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" className="ms-2">
           <Plus className="h-4 w-4" />
         </Button>
       </SheetTrigger>
--- features/pokemon/components/search-input.tsx
+++ features/pokemon/components/search-input.tsx
@@ -7,20 +7,22 @@ export function SearchInput() {
   const { searchQuery, setSearchQuery } = usePokedexStore();
 
   return (
-    <div className="relative flex gap-2 items-center">
+    <div className="relative flex items-center w-full">
       <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-      <Input
-        placeholder="Search Pokémon..."
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className="pl-8 pr-8 bg-white w-full"
-      />
-      {searchQuery && (
-        <X
-          className="absolute right-2 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground cursor-pointer"
-          onClick={() => setSearchQuery('')}
+      <div className="relative w-full">
+        <Input
+          placeholder="Search Pokémon..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="pl-8 pr-8 bg-white w-full"
         />
-      )}
+        {searchQuery && (
+          <X
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground cursor-pointer"
+            onClick={() => setSearchQuery('')}
+          />
+        )}
+      </div>
       <LocalPokemonForm />
     </div>
   );
